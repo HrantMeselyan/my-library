7ee540614ea4178e79c9e1c83f924b2b3dd48103
dell <hrant3307@gmail.com>
2023-05-01 21:01:09 +0400

    library

--- src/main/java/com/example/authorBookServlet/filter/UserAuthorAction.java
+++ src/main/java/com/example/authorBookServlet/filter/UserAuthorAction.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/authors","/updateAuthor","/createAuthor"})
+@WebFilter(urlPatterns = {"/updateAuthor","/createAuthor","/removeAuthor"})
 public class UserAuthorAction implements Filter {
 
     @Override
